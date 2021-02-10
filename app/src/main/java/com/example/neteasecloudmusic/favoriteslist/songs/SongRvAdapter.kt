aa744584bb5dabe2757fb44513496cc07e600d1b
False-Mask <2623036785@qq.com>
2021-02-10 14:17:15 +0800

    '修改了歌单列表rv中最后一个子item不显示的问题,(备份一下先学学MediaPlayer和Service)'

--- app/src/main/java/com/example/neteasecloudmusic/favoriteslist/songs/SongRvAdapter.kt
+++ app/src/main/java/com/example/neteasecloudmusic/favoriteslist/songs/SongRvAdapter.kt
@@ -8,40 +8,43 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.neteasecloudmusic.R
-import com.example.neteasecloudmusic.recyclerview.favorites.list
 import kotlinx.android.synthetic.main.rv_song_list_item.view.*
 
 var songList: MutableList<Song> = mutableListOf()
 class SongRvAdapter : RecyclerView.Adapter<SongRvAdapter.Holder>() {
-    private lateinit var view:View
+    lateinit var view: View
+
     //必须初始化
-    lateinit var listener:OnClickListener
+    lateinit var listener: OnClickListener
+
+    //var mPosition:Int = 0
 
     //内部类
-    class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        lateinit var view:View
+    class Holder(itemView: View) : RecyclerView.ViewHolder(itemView){
         lateinit var songPosition:TextView
+        lateinit var view:View
         lateinit var songName:TextView
         lateinit var singerName:TextView
-
-
         init {
+            //不知道为啥貌似每一个布局都会
             try {
+
                 view=itemView
-                songPosition=itemView?.song_position
-                songName=itemView?.song_name
-                singerName=itemView?.singer_name
+                songPosition = itemView.song_position
+                singerName=itemView.song_name
+                songName=itemView.singer_name
             }catch (e:Exception){
-                Log.e("RV Holder", ": (多半是空指针异常)",e )
+                Log.e("Holder", ":异常 ", e)
             }
         }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        view=when(viewType){
-            MyViewType.SongView.ordinal->LayoutInflater.from(parent.context).inflate(R.layout.rv_song_list_item,parent,false)
-            MyViewType.FirstView.ordinal->LayoutInflater.from(parent.context).inflate(R.layout.rv_song_first,parent,false)
-            else->LayoutInflater.from(parent.context).inflate(R.layout.rv_song_last,parent,false)
+        view = when (viewType) {
+            MyViewType.SongView.ordinal -> LayoutInflater.from(parent.context).inflate(R.layout.rv_song_list_item, parent, false)
+            MyViewType.FirstView.ordinal -> LayoutInflater.from(parent.context).inflate(R.layout.rv_song_first, parent, false)
+            else -> LayoutInflater.from(parent.context).inflate(R.layout.rv_song_last, parent, false)
         }
         return Holder(view)
     }
@@ -51,64 +54,64 @@ class SongRvAdapter : RecyclerView.Adapter<SongRvAdapter.Holder>() {
     }
 
     override fun getItemViewType(position: Int): Int {
-       return when(position){
-           0-> MyViewType.FirstView.ordinal
-           list.size-1->MyViewType.LastView.ordinal
-           else->MyViewType.SongView.ordinal
-       }
+        //mPosition=position
+        return when (position) {
+            0 -> MyViewType.FirstView.ordinal
+            songList.size - 1 -> MyViewType.LastView.ordinal
+            else -> MyViewType.SongView.ordinal
+        }
     }
 
     override fun onBindViewHolder(holder: SongRvAdapter.Holder, position: Int) {
         //改变视图
 
-        when(position){
+        when (position) {
             //标题栏
-            0->{
+            0 -> {
                 holder.view.setOnClickListener {
                     listener.titleClicked(view)
                 }
             }
             //最后的项目
-            songList.size-1 ->{
+            songList.size - 1 -> {
                 holder.view?.setOnClickListener {
                     listener.tailClicked(view)
-                    Log.e("最后一个白板", "onBindViewHolder: " )
+                    Log.e("最后一个白板", "onBindViewHolder: ")
                 }
             }
             //song
-            else->{
-                    holder.singerName.text = songList[position].artist
-                    holder.songName.text= songList[position].songName
-                    holder.songPosition.text=position.toString()
-                    holder.view.setOnClickListener{
-                        listener.songClicked(view,position)
-                    }
+            else -> {
+                holder.singerName.text = songList[position].artist
+                holder.songName.text = songList[position].songName
+                holder.songPosition.text = position.toString()
+                holder.view.setOnClickListener {
+                    listener.songClicked(view, position)
+                }
             }
         }
     }
 
     //
-    fun setList(list: MutableList<Song>){
+    fun setList(list: MutableList<Song>) {
         //头是前面的head
         //尾是结束白框框
         list.add(0, Song())
         list.add(Song())
-        songList=list
+        songList = list
     }
 
     //添加Click的接口回调
-    fun setOnItemClickListener(mListener: OnClickListener){
-        this.listener=mListener
+    fun setOnItemClickListener(mListener: OnClickListener) {
+        this.listener = mListener
     }
 
     interface OnClickListener {
-        fun songClicked(view:View,position: Int)
-        fun titleClicked(view:View)
-        fun tailClicked(view:View)
+        fun songClicked(view: View, position: Int)
+        fun titleClicked(view: View)
+        fun tailClicked(view: View)
     }
 
-    enum class MyViewType{
-        FirstView,SongView,LastView
+    enum class MyViewType {
+        FirstView, SongView, LastView
     }
-
 }
--- app/src/main/java/com/example/neteasecloudmusic/favoriteslist/songui/SongUiActivity.kt
+++ app/src/main/java/com/example/neteasecloudmusic/favoriteslist/songui/SongUiActivity.kt
@@ -1,12 +1,50 @@
 package com.example.neteasecloudmusic.favoriteslist.songui
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.example.neteasecloudmusic.R
+import com.example.neteasecloudmusic.favoriteslist.songs.Song
+import kotlinx.android.synthetic.main.activity_song_ui.*
+import kotlinx.android.synthetic.main.rv_song_list_item.*
 
 class SongUiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_song_ui)
+        //获取activity的数据
+        var song=intent.extras?.get("song") as Song
+        var position=intent.extras?.getInt("position")
+        //初始化视图了
+        initView(song,position)
+
+        //设置点击事件的处理
+        //返回键
+        song_ui_back_icon.setOnClickListener{
+            finish()
+        }
+        //  播放/暂停键
+        play_pause_icon.setOnClickListener{
+
+        }
+        //下一首
+        the_next_song_icon.setOnClickListener{
+
+        }
+
+        //上一首
+        the_last_song_icon.setOnClickListener{
+
+        }
+    }
+
+    private fun initView(song: Song, position: Int?) {
+        //初始化text
+        song_ui_song_name.text=song.songName
+        song_ui_singer_name.text=song.artist
+        //加载图片
+        Glide.with(this).load(song.image).into(song_ui_song_image_view)
     }
 }
